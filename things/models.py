251020769e022a942275ed4722684feb35d6781e
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-16 17:14:39 +0100

    Complete exercise 3.
    Add constraints to atrributes name, description and quantity in class Thing in models.py.

--- things/models.py
+++ things/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):   # overriding default user model from django
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True, max_length=120)
+    quantity = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
