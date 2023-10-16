593fb76b8212c07189dfccb546a914b52a89ef7d
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-16 17:10:28 +0100

    Complete exercise 2.
    Add Thing class to models.py with attributes name, description and quantity.

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):   # overriding default user model from django
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
