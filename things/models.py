c8e735ae2c55ae9e924a529a6d90e28c89b5e2d6
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-16 17:27:16 +0100

    Fix exercise 3 and commit migrations

--- things/models.py
+++ things/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
-class Thing(models.Model):   # overriding default user model from django
-    name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, max_length=120)
-    quantity = models.PositiveSmallIntegerField(
-        default=0,
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
+class Thing(models.Model):
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30
+    )
+    description =  models.CharField(
+        unique=False,
+        blank=True,
+        max_length=120
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[MaxValueValidator(100),
+                    MinValueValidator(0)]
     )
